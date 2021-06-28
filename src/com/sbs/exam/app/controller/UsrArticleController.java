c37ea012762c3b6030d50a5566559290015c7532
jhs512 <jangka512@gmail.com>
2021-06-28 11:28:54 +0900

    UsrArticleController 내부의 로직을 여러 액션메서드 들로 나누기

--- src/com/sbs/exam/app/controller/UsrArticleController.java
+++ src/com/sbs/exam/app/controller/UsrArticleController.java
@@ -20,6 +20,10 @@ public class UsrArticleController {
 		articlesLastId = 0;
 
 		// 테스트 게시물 만들기
+		makeTestData();
+	}
+
+	private void makeTestData() {
 		for (int i = 0; i < 10; i++) {
 			Article article = new Article();
 			article.id = articlesLastId + 1;
@@ -32,97 +36,6 @@ public class UsrArticleController {
 		}
 	}
 
-	public void performAction(Rq rq) {
-		if (rq.getActionPath().equals("/usr/article/write")) {
-			System.out.printf("제목 : ");
-			String title = sc.nextLine().trim();
-			System.out.printf("내용 : ");
-			String body = sc.nextLine().trim();
-
-			Article article = new Article();
-			article.id = articlesLastId + 1;
-			article.regDate = Util.getNowDateStr();
-			article.updateDate = Util.getNowDateStr();
-			article.title = title;
-			article.body = body;
-			articles.add(article);
-
-			articlesLastId++;
-
-			System.out.printf("%d번 게시물이 생성되었습니다.\n", article.id);
-		} else if (rq.getActionPath().equals("/usr/article/list")) {
-			System.out.printf("번호 / 작성날자 / 제목\n");
-
-			for (int i = articles.size() - 1; i >= 0; i--) {
-				Article article = articles.get(i);
-				System.out.printf("%d / %s / %s\n", article.id, article.regDate, article.title);
-			}
-
-		} else if (rq.getActionPath().equals("/usr/article/detail")) {
-			int id = rq.getIntParam("id", 0);
-
-			if (id == 0) {
-				System.out.println("id를 입력해주세요.");
-				return;
-			}
-
-			Article foundArticle = getArticleById(id);
-
-			if (foundArticle == null) {
-				System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
-				return;
-			}
-
-			System.out.printf("번호 : %d\n", foundArticle.id);
-			System.out.printf("작성날짜 : %s\n", foundArticle.regDate);
-			System.out.printf("수정날짜 : %s\n", foundArticle.updateDate);
-			System.out.printf("제목 : %s\n", foundArticle.title);
-			System.out.printf("내용 : %s\n", foundArticle.body);
-
-		} else if (rq.getActionPath().equals("/usr/article/delete")) {
-			int id = rq.getIntParam("id", 0);
-
-			if (id == 0) {
-				System.out.println("id를 입력해주세요.");
-				return;
-			}
-
-			Article foundArticle = getArticleById(id);
-
-			if (foundArticle == null) {
-				System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
-				return;
-			}
-
-			articles.remove(foundArticle);
-
-			System.out.printf("%d번 게시물을 삭제하였습니다.\n", id);
-
-		} else if (rq.getActionPath().equals("/usr/article/modify")) {
-			int id = rq.getIntParam("id", 0);
-
-			if (id == 0) {
-				System.out.println("id를 입력해주세요.");
-				return;
-			}
-
-			Article foundArticle = getArticleById(id);
-
-			if (foundArticle == null) {
-				System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
-				return;
-			}
-
-			System.out.printf("새 제목 : ");
-			foundArticle.title = sc.nextLine();
-			System.out.printf("새 내용 : ");
-			foundArticle.body = sc.nextLine();
-			foundArticle.updateDate = Util.getNowDateStr();
-
-			System.out.printf("%d번 게시물을 수정하였습니다.\n", id);
-		}
-	}
-
 	private Article getArticleById(int id) {
 		for (Article article : articles) {
 			if (article.id == id) {
@@ -132,4 +45,114 @@ public class UsrArticleController {
 
 		return null;
 	}
+
+	public void performAction(Rq rq) {
+		if (rq.getActionPath().equals("/usr/article/write")) {
+			actionWrite(rq);
+		} else if (rq.getActionPath().equals("/usr/article/list")) {
+			actionList(rq);
+		} else if (rq.getActionPath().equals("/usr/article/detail")) {
+			actionDetail(rq);
+		} else if (rq.getActionPath().equals("/usr/article/delete")) {
+			actionDelete(rq);
+
+		} else if (rq.getActionPath().equals("/usr/article/modify")) {
+			actionModify(rq);
+		}
+	}
+
+	private void actionModify(Rq rq) {
+		int id = rq.getIntParam("id", 0);
+
+		if (id == 0) {
+			System.out.println("id를 입력해주세요.");
+			return;
+		}
+
+		Article foundArticle = getArticleById(id);
+
+		if (foundArticle == null) {
+			System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+			return;
+		}
+
+		System.out.printf("새 제목 : ");
+		foundArticle.title = sc.nextLine();
+		System.out.printf("새 내용 : ");
+		foundArticle.body = sc.nextLine();
+		foundArticle.updateDate = Util.getNowDateStr();
+
+		System.out.printf("%d번 게시물을 수정하였습니다.\n", id);
+	}
+
+	private void actionDelete(Rq rq) {
+		int id = rq.getIntParam("id", 0);
+
+		if (id == 0) {
+			System.out.println("id를 입력해주세요.");
+			return;
+		}
+
+		Article foundArticle = getArticleById(id);
+
+		if (foundArticle == null) {
+			System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+			return;
+		}
+
+		articles.remove(foundArticle);
+
+		System.out.printf("%d번 게시물을 삭제하였습니다.\n", id);
+	}
+
+	private void actionDetail(Rq rq) {
+		int id = rq.getIntParam("id", 0);
+
+		if (id == 0) {
+			System.out.println("id를 입력해주세요.");
+			return;
+		}
+
+		Article foundArticle = getArticleById(id);
+
+		if (foundArticle == null) {
+			System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+			return;
+		}
+
+		System.out.printf("번호 : %d\n", foundArticle.id);
+		System.out.printf("작성날짜 : %s\n", foundArticle.regDate);
+		System.out.printf("수정날짜 : %s\n", foundArticle.updateDate);
+		System.out.printf("제목 : %s\n", foundArticle.title);
+		System.out.printf("내용 : %s\n", foundArticle.body);
+	}
+
+	private void actionList(Rq rq) {
+		System.out.printf("번호 / 작성날자 / 제목\n");
+
+		for (int i = articles.size() - 1; i >= 0; i--) {
+			Article article = articles.get(i);
+			System.out.printf("%d / %s / %s\n", article.id, article.regDate, article.title);
+		}
+	}
+
+	private void actionWrite(Rq rq) {
+		System.out.printf("제목 : ");
+		String title = sc.nextLine().trim();
+		System.out.printf("내용 : ");
+		String body = sc.nextLine().trim();
+
+		Article article = new Article();
+		article.id = articlesLastId + 1;
+		article.regDate = Util.getNowDateStr();
+		article.updateDate = Util.getNowDateStr();
+		article.title = title;
+		article.body = body;
+		articles.add(article);
+
+		articlesLastId++;
+
+		System.out.printf("%d번 게시물이 생성되었습니다.\n", article.id);
+	}
+
 }
